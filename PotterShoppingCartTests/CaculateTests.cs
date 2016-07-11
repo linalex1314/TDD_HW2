734d67a854429be03feacf71658f94bd2017585c
linalex1314 <linalex1314@gmail.com>
2016-07-11 11:33:04 +0800

    測試案例2=>Scenario: 第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190,亮紅燈

--- PotterShoppingCartTests/CaculateTests.cs
+++ PotterShoppingCartTests/CaculateTests.cs
@@ -7,22 +7,15 @@ namespace PotterShoppingCart.Tests
     public class CaculateTests
     {
         [TestMethod()]
-        public void Buy_1_book_price_should_be_100()
+        public void Buy_book1_price_should_be_100()
         {
             //arrange
             var target = new Caculate();
-
-            var bookRepository = new List<Book> {
-                new Book { Name="Book1",Price=100},
-                new Book { Name="Book2",Price=100},
-                new Book { Name="Book3",Price=100},
-                new Book { Name="Book4",Price=100},
-                new Book { Name="Book5",Price=100}
-                };
+            var books = GetBooks();
 
             var shoppingCart = new ShoppingCart();
 
-            shoppingCart.Add(bookRepository.Find(x => x.Name == "Book1"));
+            shoppingCart.Add(books.Find(x => x.Name == "Book1"));
 
             //act
             var actual = target.CaculatePrice(shoppingCart);
@@ -30,6 +23,39 @@ namespace PotterShoppingCart.Tests
 
             //assert
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Buy_book1_book2_price_should_be_100multipliedBy2multipliedByPercent95_result_is_190()
+        {
+            //arrange
+            var target = new Caculate();
+            var books = GetBooks();
+
+            var shoppingCart = new ShoppingCart();
+
+            shoppingCart.Add(books.Find(x => x.Name == "Book1"));
+            shoppingCart.Add(books.Find(x => x.Name == "Book2"));
+
+            //act
+            var actual = target.CaculatePrice(shoppingCart);
+            var expected = 190;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        private List<Book> GetBooks()
+        {
+            var books = new List<Book> {
+                new Book { Name="Book1",Price=100},
+                new Book { Name="Book2",Price=100},
+                new Book { Name="Book3",Price=100},
+                new Book { Name="Book4",Price=100},
+                new Book { Name="Book5",Price=100}
+                };
+
+            return books;
         }
     }
 }
