18286f1d1f7077b8bcd65db2165ac303c9abf454
linalex1314 <linalex1314@gmail.com>
2016-07-11 17:28:30 +0800

    測試案例 2=>Scenario: 通過, 綠燈

--- PotterShoppingCart/Caculate.cs
+++ PotterShoppingCart/Caculate.cs
@@ -8,6 +8,8 @@ namespace PotterShoppingCart
 {
     public class Caculate
     {
+        public List<Book> bookSet { get; set; }
+
         public int CaculateShipping()
         {
             return 100;
@@ -17,9 +19,23 @@ namespace PotterShoppingCart
         {
             int totalPrice = 0;
 
-            if (shoppingCart.books.Count == 1)
+            double buy1book = 1.0;
+            double buy2book = 0.95;
+            double buy3book = 0.9;
+            double buy4book = 0.8;
+            double buy5book = 0.75;
+
+            //var bookSet = shoppingCart.books
+            //              .GroupBy(x => x.Name)
+            //              .Select(g => new { Name = g.Key, Count = g.Count() });
+
+            if (shoppingCart.books.Exists(x => x.Name == "Book1") && shoppingCart.books.Exists(x => x.Name == "Book2"))
             {
-                totalPrice = 100;
+                totalPrice = (int)(2 * 100 * buy2book);
+            }
+            else if (shoppingCart.books.Exists(x => x.Name == "Book1"))
+            {
+                totalPrice = (int)(1 * 100 * buy1book);
             }
 
             return totalPrice;
